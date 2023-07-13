a7772b580320720d34bc8de09b2d45d5419453fe
YabZewd <yabse.z@gmail.com>
2023-07-13 14:44:21 -0700

    C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,20 @@
 /*
- *the program prints out different sizes of data types
+ * File name is 6-size.c
  */
 
 #include <stdio.h>
+
+/**
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
+ *
+ * Return: 0.
+ */
 
 int main(void)
 {
 	printf("size of char: %zu bytes" , sizeof(char));
 	printf("size of int: %zu bytes" , sizeof(int));
-	printf("size of double: %zu bytes" , sizeof(double));
 	printf("size of float: %zu bytes" , sizeof(float));
 	printf("size of long int: %zu bytes" , sizeof(long int));
 	printf("size of long long int: %zu bytes" , sizeof(long long int));
