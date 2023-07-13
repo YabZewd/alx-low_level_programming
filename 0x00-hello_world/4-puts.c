046cb4295a5009911caac70316a3f72b887c1d32
YabZewd <yabse.z@gmail.com>
2023-07-13 14:21:42 -0700

    C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,22 @@
 /*
- * This program outputs programming is like building a multilingual puzzle
+ * This program outputs programming
+ * is like building a multilingual puzzle
  */
 /*a header file to use some predefined functions*/
 
 #include <stdio.h>
 
 /**
- * there shoukd always be a main function and it should return 0
+ * there shoukd always be a
+ * main function and it should return 0
  */
 
 int main(void)
 {
+	/*
+	 *the main function outputs programming is like
+	 *a multilingual puzzle
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
