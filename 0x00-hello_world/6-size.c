cced0aa6c8fe2cf5bc94f9624b672ae1dc9e156c
YabZewd <yabse.z@gmail.com>
2023-07-13 14:59:46 -0700

    C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -21,9 +21,9 @@ int main(void)
 
 	printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(c));
 
 	return (0);
 }
