1c45acbb64a016513ace7dcc8dbb0da88d144d36
YabZewd <yabse.z@gmail.com>
2023-07-13 14:27:49 -0700

    C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,17 @@
 /*
- * The file's name is 4-puts.c
+ * File: 4-puts.c
  */
-
 
 #include <stdio.h>
 
-/*
- *the main function outputs programming is like
- *a multilingual puzzle
-
- * main returns return 0
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: 0.
  */
-
 int main(void)
 {
-	/*
-	 *the main function outputs programming is like
-	 *a multilingual puzzle
-	 */
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
